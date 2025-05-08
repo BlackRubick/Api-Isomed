3695a05b9cd5c4a504f5157c429ee82b1d8a5511
Ubuntu <ubuntu@ip-172-31-86-212.ec2.internal>
2025-05-08 05:04:32 +0000

    lol

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 import uvicorn
+import ssl
 from app.infrastructure.api.server import app
 from app.infrastructure.config.settings import Settings
 
 settings = Settings()
 
 if __name__ == "__main__":
+    # Configuración SSL
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    # Rutas a tus certificados (ajústalas según donde estén ubicados)
+    cert_path = "/etc/letsencrypt/live/34.232.185.39/fullchain.pem"
+    key_path = "/etc/letsencrypt/live/34.232.185.39/privkey.pem"
+    ssl_context.load_cert_chain(cert_path, key_path)
+    
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True if settings.DEBUG else False,
+        ssl=ssl_context,
+        reload=settings.DEBUG
     )
